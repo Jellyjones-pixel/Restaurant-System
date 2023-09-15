6b931826b2305fed9fb0e1eddd3571aa0761dac5
jellyjones-pixel <johnmbithimutave@gmail.com>
2023-09-15 14:26:07 +0100

    Resturnt Sytem

---
+++ application/views/front/partials/footer.php
@@ -0,0 +1,6 @@
+<footer class="section p-3 bg-dark text-white">
+    <div class="text-center">&copy; John Mbithi <?php echo date("Y"); ?> Recipe Management System</div>
+</footer>
+</body>
+
+</html>
